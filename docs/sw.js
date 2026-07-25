50176b4833fe68170fa0f54dbc8d708aeead9a87
amineadmin9-max <amineadmin9@gmail.com>
2026-07-25 13:52:20 +0000

    Fix SW: clear all caches on activate, don't cache index.html

--- docs/sw.js
+++ docs/sw.js
@@ -1,18 +1,18 @@
-const CACHE = 'niche-finder-v2';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE = 'niche-finder-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.url.includes('raw.githubusercontent.com') || e.request.url.includes('allorigins')) {
+  if (e.request.url.includes('raw.githubusercontent.com') || e.request.url.includes('allorigins') || e.request.url.includes('corsproxy') || e.request.url.includes('codetabs')) {
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
   }
 });
